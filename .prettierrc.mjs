f73f2c0a72bec38312e5e1f60f21683b5f33e28d
Yefree Valdez <yefree13santos@outlook.com>
2024-12-06 14:47:33 -0400

    add form component

--- .prettierrc.mjs
+++ .prettierrc.mjs
@@ -3,7 +3,7 @@ export default {
   plugins: ["prettier-plugin-astro"],
   overrides: [
     {
-      files: "*.astro",
+      files: "**/*.astro",
       options: {
         parser: "astro",
       },
